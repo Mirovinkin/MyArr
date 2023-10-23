5852a4316f8dad9ab8a7c961f809b7698dbf06be
Mirovinkin <137717625+Mirovinkin@users.noreply.github.com>
2023-10-23 22:05:08 +0500

    Update ComplexNumbers.cpp

--- ComplexNumbers/ComplexNumbers.cpp
+++ ComplexNumbers/ComplexNumbers.cpp
@@ -26,7 +26,43 @@ template<typename T> T maximum(const T* arr, const T& size) {
 	}
 	return res;
 }
+template<typename T>
+auto SquareEquation(T a, T b, T c) {
+	T degree;
+	degree = pow(b, 2) - (4 * a * c);
+	
+	if (degree < 0) {
+		return "Нет корней";
+	}
+	else if (degree == 0) {
+		T x1;
+		x1 = -b / (a * 2);
+		return x1;
+	}
+	else {
+		T x1, x2;
+		x1 = (( - b) + sqrt(degree) / (a * 2));
+		x2 = (( - b) - sqrt(degree) / (a * 2));
+		pair<T, T> root{ x1,x2 };
+		return root;
+	}
+}
 
+template<typename T>
+auto LinearEquation(T a, T b) {
+	T res;
+
+	if (a != 0) {
+		res = (-b) / a;
+		return res;
+	}
+	else if (a == 0 && b != 0) {
+		 throw exception("Error");
+	}
+	else {
+		return 0;
+	}
+}
 int main()
 {
 	setlocale(LC_ALL, "RUS");
